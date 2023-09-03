2a14344c001e07bfdda8c8adad21b34808aef7a9
ssnowki <the_end68@mail.ru>
2023-09-03 10:59:26 +0300

    add programm

--- Program.cs
+++ Program.cs
@@ -2,5 +2,48 @@
 
 class Program
 {
-    
+    //Написать программу, которая из имеющегося массива строк формирует новый массив из строк, 
+    //длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры,
+     //либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями,
+      //лучше обойтись исключительно массивами.
+    static void Main(string[] args)
+    {
+        void PrintArray(string[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+                {
+                    Console.Write($"{array[i]} ");
+                }
+                Console.WriteLine();
+        }
+        
+
+        string [] str = {"1234", "156", "-2", "computer science"}; 
+        
+        int count = 0;
+
+        for (int i=0; i<str.Length;i++)
+        {
+            if (str[i].Length <4)
+            {
+                count+=1;
+            }
+        }
+        
+        string [] newStr = new string[count];
+        int j =0;
+
+        for (int i=0; i<str.Length;i++)
+        {
+            if (str[i].Length <4)
+            {
+                newStr[j]=str[i];
+                j+=1;
+            }
+        }
+        
+        PrintArray(newStr);
+
+       
+    }
 }
